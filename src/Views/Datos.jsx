70b43f23325c14c6f7c18b1466ce6b33a01170de
CesarAugusto200 <106979505+CesarAugusto200@users.noreply.github.com>
2023-07-23 17:09:22 -0600

    Update Datos.jsx
    conectado a  back en aws

--- src/Views/Datos.jsx
+++ src/Views/Datos.jsx
@@ -50,7 +50,7 @@ const Datos = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8080/data-readings');
+        const response = await fetch('44.206.238.218');
         const jsonData = await response.json();
 
         const newCrops = jsonData.map(row => {
