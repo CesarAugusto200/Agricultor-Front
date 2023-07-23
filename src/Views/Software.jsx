d5bb02dcd193916560b4637885103c085b1fccaa
CesarAugusto200 <106979505+CesarAugusto200@users.noreply.github.com>
2023-07-23 17:41:34 -0600

    Update Software.jsx
    nuevas ip asignadas

--- src/Views/Software.jsx
+++ src/Views/Software.jsx
@@ -42,7 +42,7 @@ const Software = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch('44.206.238.218'); 
+      const response = await fetch('http://54.86.0.188/data-readings'); 
       const jsonData = await response.json();
 
       const newTemperatureData = jsonData.map(data => ({
